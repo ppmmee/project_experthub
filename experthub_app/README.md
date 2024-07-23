90f937e3327b7c97d8340c2da47c9cc17763058f
ppm-mee <mju6404106322@mju.ac.th>
2024-07-23 13:54:01 +0700

    Create new flutter
    create project with flutter

---
+++ experthub_app/README.md
@@ -0,0 +1,3 @@
+# experthub_app
+
+A new Flutter project.
---
+++ experthub_app/android/app/src/main/kotlin/com/example/experthub_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.experthub_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
